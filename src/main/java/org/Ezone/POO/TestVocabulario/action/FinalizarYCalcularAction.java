a0081cdba01748f4785c6b10f17718e9b52fcb58
DiegoAGomezS <dagomez@uamv.edu.ni>
2026-06-24 21:25:09 -0600

    Correción de errores en el action

--- src/main/java/org/Ezone/POO/TestVocabulario/action/FinalizarYCalcularAction.java
+++ src/main/java/org/Ezone/POO/TestVocabulario/action/FinalizarYCalcularAction.java
@@ -1,5 +1,6 @@
 package org.Ezone.POO.TestVocabulario.action;
 
+import org.Ezone.POO.TestVocabulario.enums.EstadoIntento;
 import org.Ezone.POO.TestVocabulario.model.*;
 import org.Ezone.POO.TestVocabulario.service.*;
 import org.openxava.actions.*;
@@ -7,9 +8,21 @@ import org.openxava.jpa.*;
 
 public class FinalizarYCalcularAction extends ViewBaseAction {
 
+
+
     @Override
     public void execute() throws Exception {
+        if (getView().getEntity() == null) {
+            addError("Primero debes seleccionar o guardar un intento.");
+            return;
+        }
+
         IntentoPrueba intento = (IntentoPrueba) getView().getEntity();
+
+        if (EstadoIntento.CALIFICADO.equals(intento.getEstadoIntento())) {
+            addError("Esta prueba ya fue calificada.");
+            return;
+        }
 
         IAplicacionPruebaService appService = new AplicacionPruebaService();
         ICalculoResultadoService calcService = new CalculoResultadoService();
